a3e7ab270b7fbdf6297c622d49095bc37a9d13ed
shuliqi <1169046371@qq.com>
2016-04-10 14:38:59 +0800

    旧版本的博客，保存一份

---
+++ oldBlog/js/search.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>search</title>
+	<meta charset="utf-8">
+</head>
+<style type="text/css">
+html,body{
+	width: 100%;
+	height: 100%;
+}
+.search_zong{
+	width: 100%;
+	height: 100%;
+	background: red;
+}
+</style>
+<body>
+<div class="search_zong">
+	<form>
+		<input type="text" />
+		<input type="submit" />
+	</form>
+</div>
+</body>
+</html>
